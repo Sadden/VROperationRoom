4a17af33c19b60af96f57efebe025ed521d2bafb
luke <lilingyu0217@126.com>
2018-03-20 17:51:30 -0400

    PunConnectionTesting

---
+++ Assets/PUN/Script/PhotonConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Photon;
+using UnityEngine;
+
+public class PhotonConnection : PunBehaviour {
+    string temp;
+
+	
+	void Start () {
+        temp = PhotonNetwork.connectionStateDetailed.ToString();
+        Debug.Log(temp);
+
+        PhotonNetwork.ConnectUsingSettings("1.0"); //connect photon server
+	}
+	
+	
+	void Update () {
+		if(temp!=PhotonNetwork.connectionStateDetailed.ToString())
+        {
+            temp = PhotonNetwork.connectionStateDetailed.ToString();
+            Debug.Log(temp);
+        }
+	}
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();//run default lobby when connected to master server.
+    }
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.CreateRoom("");//create room and join room automatically when join lobby
+    }
+}
